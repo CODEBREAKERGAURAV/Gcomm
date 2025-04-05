2e7e92bb0cd3bacedb96ce87b27bad278cb3e66e
CODEBREAKERGAURAV <gy3098225@gmail.com>
2025-04-06 00:15:21 +0530

    1.4

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './sections/Header'
-import Hero from './sections/hero'
+import Hero from './sections/Hero'
 import Category from './sections/Category'
 import Services from './sections/Services'
 import Productsgrid from './sections/Productsgrid'
