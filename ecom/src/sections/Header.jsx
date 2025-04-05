6feaed96ed877e12f42b46ab95b28b5b983e28b6
CODEBREAKERGAURAV <gy3098225@gmail.com>
2025-04-05 23:12:04 +0530

    1.3

--- ecom/src/sections/Header.jsx
+++ ecom/src/sections/Header.jsx
@@ -48,10 +48,10 @@ const Header = () => {
     <div className="w-full px-16 py-2 bg-yellow-400 lg:flex hidden justify-between items-center gap-6">
       <h1 className="text-sm font-semibold flex justify-center items-center gap-2"><FaPhoneVolume className="size-[18px]"/><span>+91 6387412528</span></h1>
       <h1 className="text-sm font-semibold flex justify-center items-center gap-2"><FaMapMarkerAlt className="size-[18px]"/><span>MuradNagar</span></h1>
-      <h1 className="text-sm font-semibold flex justify-center items-center gap-2"><MdEmail className="size-[18px]"/><span>xyz@gmail.com</span></h1>
+      <h1 className="text-sm font-semibold flex justify-center items-center gap-2"><MdEmail className="size-[18px]"/><span>Gaurav@gmail.com</span></h1>
     </div>
     <nav className="w-full bg-gray-100 flex justify-between items-center gap-1 lg:px-16 px-6 py-5 sticky top-0 z-50">
-      <h1 className="text-purple-500 font-bold lg:text-[30px] text-3xl underline italic ">Gcomm</h1>
+      <h1 className="text-purple-500 font-bold lg:text-[30px] text-3xl underline italic ">Gaurav-comm</h1>
       <ul className="lg:flex justify-center items-center gap-10 hidden">
 
         {navItems.map(({link,path})=>(
